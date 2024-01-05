7645f86bb32ebe5742ad0a97ac3d380ac4852ac0
yuli-r <iuliana.rotaru234@gmail.com>
2024-01-05 18:03:34 +0000

    Tidy up post controller

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,22 +1,24 @@
 const asyncHandler = require('express-async-handler');
 const Post = require('../models/Post');
 
+// GET all posts
+
 exports.getPosts = asyncHandler(async (req, res, next) => {
-  try {
-    const allPosts = await Post.find();
-    res.json(allPosts);
-  } catch (err) {
-    res.status(200).json({ message: err.message });
-  }
+  allPosts = await Post.find();
+  res.json(allPosts);
 });
+
+// GET details for specific post
 
 exports.getPostDetail = asyncHandler(async (req, res, next) => {
   const post = await Post.findById(req.params.postID);
-  if (post == null) {
+  if (!post) {
     return res.status(404).json({ message: 'Post does not exist.' });
   }
-  res.json(post);
+  res.status(200).json(post);
 });
+
+// CREATE new post
 
 exports.createPost = asyncHandler(async (req, res, next) => {
   const post = new Post({
@@ -25,42 +27,42 @@ exports.createPost = asyncHandler(async (req, res, next) => {
     timestamp: new Date(),
   });
   try {
-    const newPost = await post.save();
-    res.status(201).json(newPost);
+    await post.save();
+    res.status(201).json(post);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
+
+// UPDATE post
 
 exports.updatePost = asyncHandler(async (req, res, next) => {
   try {
-    let post = await Post.findById(req.params.postID);
-    if (post == null) {
+    const post = await Post.findByIdAndUpdate(req.params.postID, req.body, {
+      new: true,
+    });
+
+    if (!post) {
       return res.status(404).json({ message: 'Post does not exist.' });
     }
 
-    if (req.body.title) {
-      post.title = req.body.title;
-    }
-    if (req.body.text) {
-      post.text = req.body.text;
-    }
-
-    await post.save();
-    res.json({ message: 'Post updated successfully.', post });
+    res.json({ message: 'Post updated successfully.', updatedPost: post });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
 
+// DELETE post
+
 exports.deletePost = asyncHandler(async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.postID);
-    if (post == null) {
+    const post = await Post.findByIdAndDelete(req.params.postID);
+
+    if (!post) {
       return res.status(404).json({ message: 'Post does not exist.' });
     }
-    await post.deleteOne();
-    res.json({ message: 'Post deleted successfully.' });
+
+    res.status(200).json({ message: 'Post deleted successfully.' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
